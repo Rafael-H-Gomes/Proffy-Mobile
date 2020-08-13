1a12ad66e0f017c8eb80fea2f455df5c0786eceb
Rafael-H-Gomes <rafaelhgomes15@gmail.com>
2020-08-12 23:12:28 -0300

    navigation tabs adicionadas

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import giveClasssesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
 
 function GiveClassses() {
-    return <View />
+    const { goBack } = useNavigation();
+
+    function handleNavigationBack() {
+        goBack();
+    }
+    
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                resizeMode= 'contain'
+                source={giveClasssesBgImage}
+                style={styles.content}
+            >
+
+              <Text style={styles.title}>Quer ser um Proffy</Text>
+              <Text style={styles.description}>
+                  Para começar, você precisa se cadastrar como professor na nossa plataforma web. 
+              </Text>
+            </ImageBackground>
+
+            <RectButton onPress={handleNavigationBack} style={styles.okButton}>
+                <Text style={styles.okButtonText}>Tudo bem</Text>
+            </RectButton>
+        </View>
+    );
 }
 
 export default GiveClassses;
